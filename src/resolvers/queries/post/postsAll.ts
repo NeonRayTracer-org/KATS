d7bfc77323c0ccc0fdacb1c23b93e8b6e7f3acfa
Mathias Wøbbe <mathias@kandelborg.dk>
2019-04-01 15:40:30 +0200

    fix(tslint): dont lint any generated files
    fix linting issues in feed & postsAll resolvers

--- src/resolvers/queries/post/postsAll.ts
+++ src/resolvers/queries/post/postsAll.ts
@@ -4,8 +4,8 @@ import { Context } from '../../../db-types'
 export const postsAll = extendType({
   definition: (t) => {
     t.field('postsAll', {
-      nullable: true,
       list: true,
+      nullable: true,
       resolve: async (_, _args, ctx: Context) => ctx.prisma.posts(),
 
       type: 'Post'
