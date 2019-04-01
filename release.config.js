09369a8dc601455dac50929dbac9cba860d8cdf1
Mathias Wøbbe <mathias@kandelborg.dk>
2019-04-01 16:02:18 +0200

    fix(semantic-versioning): add config file + deps

---
+++ release.config.js
@@ -0,0 +1,18 @@
+/* eslint-disable no-useless-escape */
+module.exports = {
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/changelog',
+    '@semantic-release/github',
+    [
+      '@semantic-release/git',
+      {
+        message:
+          // eslint-disable-next-line no-template-curly-in-string
+          'Release <%= nextRelease.version %> - <%= new Date().toLocaleDateString("en-US", {year: "numeric", month: "short", day: "numeric", hour: "numeric", minute: "numeric" }) %> [skip ci]\n\n<%= nextRelease.notes %>'
+      }
+    ]
+  ],
+  preset: 'angular'
+}
