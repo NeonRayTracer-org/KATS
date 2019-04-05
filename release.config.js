ec5f430fefa54590294810bf9d45d57bbb26e312
Mathias Wøbbe <mathias@kandelborg.dk>
2019-04-05 11:28:49 +0200

    ci(semantic-release): fix package.json version not getting updated

--- release.config.js
+++ release.config.js
@@ -4,6 +4,12 @@ module.exports = {
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
     '@semantic-release/changelog',
+    [
+      '@semantic-release/npm',
+      {
+        npmPublish: false
+      }
+    ],
     '@semantic-release/github',
     [
       '@semantic-release/git',
